cba4843d51c7e786bf6d6a70712713797a99786b
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-07-24 13:04:38 +0300

    update env

--- src/utils/env.js
+++ src/utils/env.js
@@ -5,5 +5,5 @@ export default function env(name, defaultValue) {
   if (process.env[name]) return process.env[name];
   if (defaultValue) return defaultValue;
 
-  throw new Error(`${name} variable is not found`);
+  throw new Error(`Env var with name ${name} is not found`);
 }
