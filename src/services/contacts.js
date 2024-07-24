c404ccdecb7f719b7338e55d0ece07944afe8263
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-07-24 13:12:52 +0300

    update commit

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,21 +1,5 @@
 import ContactsCollection from '../db/models/contact.js';
 
-// export async function getAllContacts() {
-//   try {
-//     return await ContactsCollection.find({});
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// export async function getContactById(id) {
-//   try {
-//     return await ContactsCollection.findById(id);
-//   } catch (error) {
-//     console.error(error);
-//     return null;
-//   }
-// }
 export async function getAllContacts() {
   return await ContactsCollection.find({});
 }
