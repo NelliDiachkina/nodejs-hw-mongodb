6d22cfc5c60d51053f45bc4f2a3c1cff6d523759
Nelli Diachkina <nelli.diachkina@gmail.com>
2024-07-24 13:55:45 +0300

    update

--- src/utils/ctrlWrapper.js
+++ src/utils/ctrlWrapper.js
@@ -1,9 +1,7 @@
-export const ctrlWrapper = (controller) => {
-  return async (req, res, next) => {
-    try {
-      await controller(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
+export const ctrlWrapper = (controller) => async (req, res, next) => {
+  try {
+    await controller(req, res, next);
+  } catch (err) {
+    next(err);
+  }
 };
